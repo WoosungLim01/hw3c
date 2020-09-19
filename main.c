0ef5fddf2ba950607230a732e484db3b91a28b17
Woosung Lim <wml5207@psu.edu>
2020-09-19 13:18:52 +0000

    end function

--- main.c
+++ main.c
@@ -1,13 +1,22 @@
+// Author: Woosung Lim wml5207@psu.edu
 #include <stdio.h>
 #include <stdlib.h>
 #include <readline/readline.h>
 
 int digit_sum(int n){
-  return n == 0 ? 0 : n%10 + digit_sum(n/10) ;
+  if (n == 0){
+    return 0 ;
+  } 
+  else{
+    return n%10 + digit_sum(n/10) ;
+  }
 }
+
 int main(void) {
   char *num = readline("Enter an int: ");
   int numinput = atoi(num);
-  printf("sum of digits of %d is %d", numinput, digit_sum(numinput));
+  printf("sum of digits of %d is %d.\n", numinput, digit_sum(numinput));
   return 0;
 }
+
+
